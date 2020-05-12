3635b90aa9684807d06f0d9efcf4c33d65aa4a32
jerbrune <jerbrune@gmail.com>
2020-05-12 11:58:15 +0200

    step 2 et 3

--- 00-Programming_for_Network_Engineers/12_6_Object-Oriented_Programming/networkDevices.py
+++ 00-Programming_for_Network_Engineers/12_6_Object-Oriented_Programming/networkDevices.py
@@ -12,5 +12,14 @@ class NetworkDevice:
         print('{0:11}{1:11}{2:18}{3:18}{4:11}'.format(self.name,self.os_type,self.ip_addr,self.user_name,self.password))
 
 
-device=NetworkDevice('Dev1','IOS_XE','10.1.1.1')
-device.print_info_device()
+def print_device_info(devices_list):
+    print('{0:11}{1:11}{2:18}{3:18}{4:11}'.format("Name","OS Type","IP address", 'user name','password'))
+    print("-"*67)
+    for device in devices_list:
+        print('{0:11}{1:11}{2:18}{3:18}{4:11}'.format(device.name,device.os_type,device.ip_addr,device.user_name,device.password))
+
+
+
+device1=NetworkDevice('Dev1','IOS_XE','10.1.1.1')
+device2=NetworkDevice('Dev2','NX-OS','20.1.1.1','Admin','C1sc0123')
+print_device_info([device1,device2])
