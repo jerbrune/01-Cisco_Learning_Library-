50fe76571f3187f2cd626253379a35a38f0e2520
jerbrune <jerbrune@gmail.com>
2020-05-12 11:48:06 +0200

    step1

--- 00-Programming_for_Network_Engineers/12_6_Object-Oriented_Programming/networkDevices.py
+++ 00-Programming_for_Network_Engineers/12_6_Object-Oriented_Programming/networkDevices.py
@@ -5,3 +5,12 @@ class NetworkDevice:
         self.ip_addr = ip_addr
         self.user_name = user
         self.password = pw
+    
+    def print_info_device(self):
+        print('{0:11}{1:11}{2:18}{3:18}{4:11}'.format("Name","OS Type","IP address", 'user name','password'))
+        print("-"*67)
+        print('{0:11}{1:11}{2:18}{3:18}{4:11}'.format(self.name,self.os_type,self.ip_addr,self.user_name,self.password))
+
+
+device=NetworkDevice('Dev1','IOS_XE','10.1.1.1')
+device.print_info_device()
