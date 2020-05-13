2d45414f19981871e10f06644db98092ddd7cc56
jerbrune <jerbrune@gmail.com>
2020-05-13 14:32:12 +0200

    commit

--- 00-Programming_for_Network_Engineers/7_Python_Data_Structures/7_11_Tuples/typles_exemple.py
+++ 00-Programming_for_Network_Engineers/7_Python_Data_Structures/7_11_Tuples/typles_exemple.py
@@ -1,1 +1,17 @@
 my_tuple = tuple()
+print(type(my_tuple))
+my_tuple = 'test','test2',"['a','b']"
+
+print(my_tuple[0])
+
+string='test'
+print(type(string))
+s=string.split()
+print(s)
+string2=str()
+print(type(string2))
+for i in string:
+    print(i)
+    string2+=i
+
+print(string2)
