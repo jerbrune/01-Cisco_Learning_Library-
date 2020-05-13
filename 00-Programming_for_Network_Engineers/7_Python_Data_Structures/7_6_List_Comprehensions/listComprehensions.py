a50a98ae9028b1e0bab3b745acd1e5a3eb1d02b2
jerbrune <jerbrune@gmail.com>
2020-05-13 08:10:48 +0200

    commit

--- 00-Programming_for_Network_Engineers/7_Python_Data_Structures/7_6_List_Comprehensions/listComprehensions.py
+++ 00-Programming_for_Network_Engineers/7_Python_Data_Structures/7_6_List_Comprehensions/listComprehensions.py
@@ -7,3 +7,12 @@ for item in device_list:
     info_list.append(item)
 
 print (info_list)
+print('')
+print('############################')
+print('#     List comprehensions  #')
+print('############################\n')
+info_list_2=[device.strip() for device in device_string.split(',') ]
+print (info_list_2)
+info_list_2.sort()
+print(info_list_2)
+
