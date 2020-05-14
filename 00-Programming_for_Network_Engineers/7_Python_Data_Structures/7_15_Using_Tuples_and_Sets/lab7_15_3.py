7dcdebb9638cb8cbbebaade4419683af73eed4b1
jerbrune <jerbrune@gmail.com>
2020-05-14 06:54:46 +0200

    coomit lab 3

--- 00-Programming_for_Network_Engineers/7_Python_Data_Structures/7_15_Using_Tuples_and_Sets/lab7_15_3.py
+++ 00-Programming_for_Network_Engineers/7_Python_Data_Structures/7_15_Using_Tuples_and_Sets/lab7_15_3.py
@@ -3,14 +3,14 @@ from pprint import pprint
 import json
 
 dev_info =namedtuple('dev_info',['name','os','ip','user','password'])
-devices_info={}
+devices_info=set()
 
 file=open('device.txt','r')
 
 for line in file:
     device_info = dev_info(*(line.strip().split(',')))
-    devices_info[device_info.name]=device_info
+    devices_info.add(device_info.os)
     
 pprint(devices_info) 
-print(json.dumps(devices_info,indent=6))
+#print(json.dumps(devices_info,indent=6))
 file.close()
