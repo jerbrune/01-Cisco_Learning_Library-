f64ea99e6950c1f9a102388811bb1f5dbaac8983
jerbrune <jerbrune@gmail.com>
2020-05-14 06:43:05 +0200

    commit lab7_15_2

--- 00-Programming_for_Network_Engineers/7_Python_Data_Structures/7_15_Using_Tuples_and_Sets/lab7_15_2.py
+++ 00-Programming_for_Network_Engineers/7_Python_Data_Structures/7_15_Using_Tuples_and_Sets/lab7_15_2.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from pprint import pprint
 
 dev_info =namedtuple('dev_info',['name','os','ip','user','password'])
 devices_info={}
@@ -7,6 +8,7 @@ file=open('device.txt','r')
 
 for line in file:
     device_info = dev_info(*(line.strip().split(',')))
-    #print(line.strip().split(','))
-print(device_info) 
+    devices_info[device_info.name]=device_info
+    
+pprint(devices_info) 
 file.close()
